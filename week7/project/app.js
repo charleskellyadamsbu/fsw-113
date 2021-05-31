fa5c49dfc2104dbb9177db4abd7078d5d1e9f2e1
Charles Adams <charleskellyadams@gmail.com>
2021-05-30 21:13:29 -0600

    Week 7 Project 1

---
+++ week7/project/app.js
@@ -0,0 +1,171 @@
+let starwars = [ {
+	"name": "Luke Skywalker",
+	"height": "172",
+	"mass": "77",
+	"hair_color": "blond",
+	"skin_color": "fair",
+	"eye_color": "blue",
+	"birth_year": "19BBY",
+	"gender": "male",
+	"homeworld": "https://swapi.dev/api/planets/1/",
+	"films": [
+		"https://swapi.dev/api/films/2/",
+		"https://swapi.dev/api/films/6/",
+		"https://swapi.dev/api/films/3/",
+		"https://swapi.dev/api/films/1/",
+		"https://swapi.dev/api/films/7/"
+	],
+	"species": [
+		"https://swapi.dev/api/species/1/"
+	],
+	"vehicles": [
+		"https://swapi.dev/api/vehicles/14/",
+		"https://swapi.dev/api/vehicles/30/"
+	],
+	"starships": [
+		"https://swapi.dev/api/starships/12/",
+		"https://swapi.dev/api/starships/22/"
+	],
+	"created": "2014-12-09T13:50:51.644000Z",
+	"edited": "2014-12-20T21:17:56.891000Z",
+	"url": "https://swapi.dev/api/people/1/"
+},
+{
+	"name": "C-3PO",
+	"height": "167",
+	"mass": "75",
+	"hair_color": "n/a",
+	"skin_color": "gold",
+	"eye_color": "yellow",
+	"birth_year": "112BBY",
+	"gender": "n/a",
+	"homeworld": "http://swapi.dev/api/planets/1/",
+	"films": [
+		"http://swapi.dev/api/films/1/",
+		"http://swapi.dev/api/films/2/",
+		"http://swapi.dev/api/films/3/",
+		"http://swapi.dev/api/films/4/",
+		"http://swapi.dev/api/films/5/",
+		"http://swapi.dev/api/films/6/"
+	],
+	"species": [
+		"http://swapi.dev/api/species/2/"
+	],
+	"vehicles": [],
+	"starships": [],
+	"created": "2014-12-10T15:10:51.357000Z",
+	"edited": "2014-12-20T21:17:50.309000Z",
+	"url": "http://swapi.dev/api/people/2/"
+},
+{
+	"name": "R2-D2",
+	"height": "96",
+	"mass": "32",
+	"hair_color": "n/a",
+	"skin_color": "white, blue",
+	"eye_color": "red",
+	"birth_year": "33BBY",
+	"gender": "n/a",
+	"homeworld": "http://swapi.dev/api/planets/8/",
+	"films": [
+		"http://swapi.dev/api/films/1/",
+		"http://swapi.dev/api/films/2/",
+		"http://swapi.dev/api/films/3/",
+		"http://swapi.dev/api/films/4/",
+		"http://swapi.dev/api/films/5/",
+		"http://swapi.dev/api/films/6/"
+	],
+	"species": [
+		"http://swapi.dev/api/species/2/"
+	],
+	"vehicles": [],
+	"starships": [],
+	"created": "2014-12-10T15:11:50.376000Z",
+	"edited": "2014-12-20T21:17:50.311000Z",
+	"url": "http://swapi.dev/api/people/3/"
+},
+{
+	"name": "Darth Vader",
+	"height": "202",
+	"mass": "136",
+	"hair_color": "none",
+	"skin_color": "white",
+	"eye_color": "yellow",
+	"birth_year": "41.9BBY",
+	"gender": "male",
+	"homeworld": "http://swapi.dev/api/planets/1/",
+	"films": [
+		"http://swapi.dev/api/films/1/",
+		"http://swapi.dev/api/films/2/",
+		"http://swapi.dev/api/films/3/",
+		"http://swapi.dev/api/films/6/"
+	],
+	"species": [],
+	"vehicles": [],
+	"starships": [
+		"http://swapi.dev/api/starships/13/"
+	],
+	"created": "2014-12-10T15:18:20.704000Z",
+	"edited": "2014-12-20T21:17:50.313000Z",
+	"url": "http://swapi.dev/api/people/4/"
+},
+{
+	"name": "Leia Organa",
+	"height": "150",
+	"mass": "49",
+	"hair_color": "brown",
+	"skin_color": "light",
+	"eye_color": "brown",
+	"birth_year": "19BBY",
+	"gender": "female",
+	"homeworld": "http://swapi.dev/api/planets/2/",
+	"films": [
+		"http://swapi.dev/api/films/1/",
+		"http://swapi.dev/api/films/2/",
+		"http://swapi.dev/api/films/3/",
+		"http://swapi.dev/api/films/6/"
+	],
+	"species": [],
+	"vehicles": [
+		"http://swapi.dev/api/vehicles/30/"
+	],
+	"starships": [],
+	"created": "2014-12-10T15:20:09.791000Z",
+	"edited": "2014-12-20T21:17:50.315000Z",
+	"url": "http://swapi.dev/api/people/5/"
+}
+];
+
+let items = document.getElementById('items');
+let submit = document.getElementById('submit');
+let range = document.getElementById('range');
+let progress = 0;
+
+async function getPerson(id) {
+    const result = await resolve(id);
+    let p = document.createElement('p');
+
+	for(let i = 0; i < 15; i++) {
+		p.textContent += JSON.stringify(result);
+	}
+
+
+    items.appendChild(p);
+}
+
+function resolve(id) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(starwars[id]);
+            range.value = ++progress;
+        }, Math.floor(Math.random() * 4500));
+    })
+}
+
+submit.addEventListener('click', (event) => {
+    getPerson(0);
+    getPerson(1);
+    getPerson(2);
+    getPerson(3);
+    getPerson(4);
+});
